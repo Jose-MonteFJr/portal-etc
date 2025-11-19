3c0f17d9e4443262c2368d472a1f6f1604ad6863
Jose-MonteFjr <zemfj11@gmail.com>
2025-11-19 20:52:51 -0300

    Inativar funcionando

--- login.php
+++ login.php
@@ -6,16 +6,32 @@ require __DIR__ . '/config/db.php';
 $email = $_POST['email'] ?? '';
 $password = $_POST['password'] ?? '';
 
+
 if (!$email || !$password) {
   header('Location: index.php?error=Preencha e-mail e senha.');
   exit;
 }
 
-$stmt = $pdo->prepare('SELECT id_usuario, nome_completo, email, password_hash, tipo, foto_perfil FROM usuario WHERE email = ? LIMIT 1');
+if ($status === 'inativo') {
+  header('Location: index.php?error=Usuário inativo, contate a secretaria.');
+  exit;
+}
+
+$stmt = $pdo->prepare('SELECT id_usuario, nome_completo, email, password_hash, tipo, foto_perfil, status FROM usuario WHERE email = ? LIMIT 1');
 $stmt->execute([$email]);
 $user = $stmt->fetch();
 
 if ($user && password_verify($password, $user['password_hash'])) {
+
+  if ($user['status'] === 'inativo') {
+    // Destrói qualquer sessão que possa ter sido iniciada por engano e redireciona
+    session_destroy();
+    header('Location: index.php?error=Usuário inativo, contate a secretaria.');
+    exit;
+  }
+
+
+
   // Guarda o essencial na sessão
   $_SESSION['id_usuario'] = $user['id_usuario'];
   $_SESSION['nome_completo'] = $user['nome_completo'];
