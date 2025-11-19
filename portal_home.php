22ad6b31750c7d46025db3419d5a32cfa4a7c4b4
Jose-MonteFjr <zemfj11@gmail.com>
2025-11-19 19:59:20 -0300

    Mais alguns ajustes feitos

--- portal_home.php
+++ portal_home.php
@@ -15,13 +15,12 @@ include __DIR__ . '/partials/portal_header.php';
 <?php flash_show(); ?>
 <!-- Conteúdo principal -->
 <div class="main">
-  <div class="content">
-    <div class="container mt-4">
-      <div class="row justify-content-center">
-        <h1 class="text-3xl font-bold">Bem vindo(a) <?php echo htmlspecialchars($userName); ?>!</h1>
-        <p class="text-gray-700 mt-4">Começar a criar a pagina principal.</p>
-      </div>
+    <div class="content">
+        <div class="container mt-4">
+            <div class="row justify-content-center">
+                <h1 class="text-3xl font-bold">Bem vindo(a) <?php echo htmlspecialchars($userName); ?>!</h1>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 <?php include __DIR__ . '/partials/footer.php'; ?>
